e09b882c06cf8fdb16128bfcb9281feb46f96340
Bob Jiang <jiang.wen@whaley.cn>
2017-02-14 14:31:54 +0800

    #new 代码生成器

---
+++ mybatis-generator-extend/src/main/java/org/mybatis/generator/extend/constant/GeneratorConstant.java
@@ -0,0 +1,22 @@
+package org.mybatis.generator.extend.constant;
+
+/**
+ * Created by Bob Jiang on 2017/2/13.
+ */
+public interface GeneratorConstant {
+
+    String BASE_MODEL_CLASS_PATH = "org.mybatis.generator.extend.model.BaseModel";
+
+    String GENERIC_MAPPER_CLASS_PATH = "org.mybatis.generator.extend.mapper.GenericMapper";
+
+    String GENERIC_SERVICE_CLASS_PATH = "org.mybatis.generator.extend.service.GenericService";
+
+    String GENERIC_SERVICE_IMPL_CLASS_PATH = "org.mybatis.generator.extend.service.impl.GenericServiceImpl";
+
+    String AUTOWIRED_CLASS_PATH = "org.springframework.beans.factory.annotation.Autowired";
+
+    String SERVICE_CLASS_PATH = "org.springframework.stereotype.Service";
+
+
+
+}
