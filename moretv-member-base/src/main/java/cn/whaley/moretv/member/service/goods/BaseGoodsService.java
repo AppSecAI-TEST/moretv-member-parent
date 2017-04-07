57c143e418b321e404c09580df080a6d6c32e926
tang.zhicheng@whaley.cn <tang.zhicheng@whaley.cn>
2017-04-07 17:10:53 +0800

    添加redisTemplate

--- moretv-member-base/src/main/java/cn/whaley/moretv/member/service/goods/BaseGoodsService.java
+++ moretv-member-base/src/main/java/cn/whaley/moretv/member/service/goods/BaseGoodsService.java
@@ -1,7 +1,12 @@
 package cn.whaley.moretv.member.service.goods;
 
+import java.util.List;
+
+import org.springframework.data.redis.core.RedisTemplate;
+
 import cn.whaley.moretv.member.base.service.GenericService;
 import cn.whaley.moretv.member.model.goods.Goods;
+import cn.whaley.moretv.member.model.member.Member;
 
 
 /**
@@ -10,8 +15,14 @@ import cn.whaley.moretv.member.model.goods.Goods;
  * 在此定义通用的Service方法，实现为BaseGoodsServiceImpl，
  * 每个前端系统中的GoodsService，都需继承此接口，实现类也需继承BaseGoodsServiceImpl
  *
- * Created by Bob Jiang on 2017/3/16.
+ * Created by  tangzc on 2017/3/16.
  */
 public interface BaseGoodsService extends GenericService<Goods, Integer> {
-
+	/**
+	 * 通过商品标号查询商品信息
+	 * @return
+	 */
+    Goods getGoodsByGoodsNo(String goodsNo);
+    
+    RedisTemplate getRedisTemplate();
 }
--- moretv-member-base/src/main/java/cn/whaley/moretv/member/service/goods/impl/BaseGoodsServiceImpl.java
+++ moretv-member-base/src/main/java/cn/whaley/moretv/member/service/goods/impl/BaseGoodsServiceImpl.java
@@ -1,5 +1,9 @@
 package cn.whaley.moretv.member.service.goods.impl;
 
+import org.springframework.data.redis.core.HashOperations;
+
+import com.alibaba.fastjson.JSON;
+
 import cn.whaley.moretv.member.base.service.impl.GenericServiceImpl;
 import cn.whaley.moretv.member.model.goods.Goods;
 import cn.whaley.moretv.member.service.goods.BaseGoodsService;
@@ -10,8 +14,19 @@ import cn.whaley.moretv.member.service.goods.BaseGoodsService;
  *
  * 在此定义多个系统都会用到的Service方法，mapper通过getGenericMapper调用
  *
- * Created by Bob Jiang on 2017/3/16.
+ * Created by tangzc on 2017/3/16.
  */
 public abstract class BaseGoodsServiceImpl extends GenericServiceImpl<Goods, Integer> implements BaseGoodsService {
 
+    @Override
+	public Goods getGoodsByGoodsNo(String goodsNo){
+    	Goods goods = null;
+    	String goodsKey = "goodsKey";
+    	HashOperations<String,String,String> ops = getRedisTemplate().opsForHash();
+    	String goodsStr = ops.get(goodsKey, goodsNo);
+    	if(goodsStr != null){
+    		goods = JSON.parseObject(goodsStr, Goods.class);
+    	}
+		return null;
+    }
 }
