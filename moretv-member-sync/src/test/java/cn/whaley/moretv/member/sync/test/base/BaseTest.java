85ec84aafb170d83f141e2a583fbe2d96204becd
Bob Jiang <jiang.wen@whaley.cn>
2017-04-11 16:35:02 +0800

    #update test

--- moretv-member-sync/src/test/java/cn/whaley/moretv/member/sync/test/base/BaseTest.java
+++ moretv-member-sync/src/test/java/cn/whaley/moretv/member/sync/test/base/BaseTest.java
@@ -1,15 +1,14 @@
 package cn.whaley.moretv.member.sync.test.base;
 
-import cn.whaley.moretv.member.sync.Application;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Created by Bob Jiang on 2016/12/22.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(Application.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class BaseTest {
 
 }
