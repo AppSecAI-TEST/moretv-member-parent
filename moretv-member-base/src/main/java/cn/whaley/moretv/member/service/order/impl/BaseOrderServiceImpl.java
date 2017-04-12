5566eda03ada86fe071528fe1f2021e2a9430e1d
tang.zhicheng@whaley.cn <tang.zhicheng@whaley.cn>
2017-04-12 17:58:02 +0800

    下单次数方法

--- moretv-member-base/src/main/java/cn/whaley/moretv/member/service/order/impl/BaseOrderServiceImpl.java
+++ moretv-member-base/src/main/java/cn/whaley/moretv/member/service/order/impl/BaseOrderServiceImpl.java
@@ -1,12 +1,15 @@
 package cn.whaley.moretv.member.service.order.impl;
 
+import java.util.Date;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.BoundValueOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import cn.whaley.moretv.member.base.service.impl.GenericServiceImpl;
+import cn.whaley.moretv.member.base.util.DateFormatUtil;
 import cn.whaley.moretv.member.mapper.order.OrderMapper;
 import cn.whaley.moretv.member.model.goods.Goods;
 import cn.whaley.moretv.member.model.order.Order;
@@ -41,7 +44,19 @@ public abstract class BaseOrderServiceImpl extends GenericServiceImpl<Order, Int
 	    order.setGoodsName(goods.getGoodsName());
 	    order.setGoodsType(goods.getGoodsType());
     	orderMapper.insert(order);
+    	
 		return order;
+    }
+    
+    public void checkCanOrderCount(Integer accountId){
+	
+    	
+    	BoundValueOperations<String,Long> opsValue = redisTemplate.boundValueOps("creteOrder"+accountId);
+    	opsValue.get();
+    	opsValue.expireAt(DateFormatUtil.addDay(1));
+    	opsValue.increment(1);
+
+    	
     }
     
 	@Override
