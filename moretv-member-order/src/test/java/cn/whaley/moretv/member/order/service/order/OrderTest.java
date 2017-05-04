4109b0a10087ee9848ad10375e649d988f936213
Bob Jiang <jiang.wen@whaley.cn>
2017-05-04 17:58:55 +0800

    #update rename

---
+++ moretv-member-order/src/test/java/cn/whaley/moretv/member/order/service/order/OrderTest.java
@@ -0,0 +1,21 @@
+package cn.whaley.moretv.member.order.service.order;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderTest {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Test
+    public void test() throws Exception {
+//        orderService.createOrder();
+    }
+}
